16be78463d649ed0d785442c292247e528401511
Shrifli <jedikyle5@gmail.com>
2023-09-21 14:12:33 -0400

    - Committed the libGDX Project Generator's result
    - Also committed the .gitignore with the Java template

---
+++ project/desktop/src/com/survivors/mygame/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.survivors.mygame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.survivors.mygame.MyGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Survivors");
+		new Lwjgl3Application(new MyGame(), config);
+	}
+}
